8ec63900b1b4e9eab9c623d1f5c5688e151f0333
Daniyar <ddr199305@gmail.com>
2017-02-20 19:49:15 +0100

    Add new route ProjectsPage

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -7,6 +7,8 @@ const Header = () => {
       <div>
         <IndexLink to="/" activeClassName="active" className="links">Home</IndexLink>
         {" | "}
+        <Link to="/projects" activeClassName="active" className="links">Projects</Link>
+        {" | "}
         <Link to="/about" activeClassName="active" className="links">About</Link>
       </div>
     </nav>
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import About from './components/about/About';
+import ProjectsPage from './components/projects/ProjectsPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
-    <About path='about' component={About} />
+    <About path="about" component={About} />
+    <ProjectsPage path="projects" component={ProjectsPage}/>
   </Route>
 );
