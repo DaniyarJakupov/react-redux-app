c90b396c2d647ba10012d3636a86c124f723b6cb
Daniyar <ddr199305@gmail.com>
2017-02-20 20:09:15 +0100

    Add bootstrap styles

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,13 +3,13 @@ import {Link, IndexLink} from 'react-router';
 
 const Header = () => {
   return(
-    <nav>
-      <div>
-        <IndexLink to="/" activeClassName="active" className="links">Home</IndexLink>
-        {" | "}
-        <Link to="/projects" activeClassName="active" className="links">Projects</Link>
-        {" | "}
-        <Link to="/about" activeClassName="active" className="links">About</Link>
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        <ul className="nav navbar-nav navbar-left">
+          <li><IndexLink to="/" activeClassName="active" className="links">Home</IndexLink></li>
+          <li><Link to="/projects" activeClassName="active" className="links">Projects</Link></li>
+          <li><Link to="/about" activeClassName="active" className="links">About</Link></li>
+        </ul>
       </div>
     </nav>
   );
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,11 +4,13 @@ import {Link} from 'react-router';
 class HomePage extends Component {
   render(){
     return(
-      <div className="jumbotron">
-        <h1>React Projects Catalog</h1>
-        <p>List of single-page web apps build with React</p>
-        <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
-      </div>
+      <header className="jumbotron">
+        <div className="container">
+          <h1>React Projects Catalog</h1>
+          <p>List of single-page web apps build with React</p>
+          <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
+        </div>
+      </header>
     )
   }
 }
