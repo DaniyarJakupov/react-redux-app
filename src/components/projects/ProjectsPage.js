dd5a9cc13fab046e30c91aca83b869e5441fbd4e
Daniyar <ddr199305@gmail.com>
2017-02-21 17:28:37 +0100

    Add state for projects

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
 
 class ProjectsPage extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      project: {title: ''}
+    };
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const project = this.state.project;
+    project.title = event.target.value;
+    this.setState({
+      project: project
+    });
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.project.title}`);
+  }
+
   render(){
     return(
       <div>
         <h1>Projects</h1>
+        <h2>Add project</h2>
+        <div className="myform">
+          <div className="form-group">
+            <input type="text" onChange={this.onTitleChange} value={this.state.project.title} className="form-control"/>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Save" onClick={this.onClickSave} className="btn btn-info btn-block"/>
+          </div>
+        </div>
       </div>
     );
   }
