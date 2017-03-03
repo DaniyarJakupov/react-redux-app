5a85fd5387d6e44471aed6abb4012a37b1ea5182
Daniyar <ddr199305@gmail.com>
2017-03-03 19:15:36 +0100

    Add redux-thunk as middleware

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,8 +1,9 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutable from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function storeConfig(inititalState) {
-  return createStore(rootReducer, inititalState, applyMiddleware(reduxImmutable()))
+  return createStore(rootReducer, inititalState, applyMiddleware(thunk, reduxImmutable()))
 }
 
