dd1447e528c04b51cbce3141ca54ccadc6f11aac
Daniyar <ddr199305@gmail.com>
2017-03-12 21:14:55 +0100

    Add a route for managing a project

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import About from './components/about/About';
 import ProjectsPage from './components/projects/ProjectsPage';
+import ManageProjectPage from './components/projects/ManageProjectPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <About path="about" component={About} />
     <ProjectsPage path="projects" component={ProjectsPage}/>
+    <ManageProjectPage path="project" component={ManageProjectPage}/>
+    <ManageProjectPage path="project/:id" component={ManageProjectPage}/>
   </Route>
 );
