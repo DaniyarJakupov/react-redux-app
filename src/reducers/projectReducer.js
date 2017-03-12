adfe9a648b1e74b2aa169503d8f2680e394370ac
Daniyar <ddr199305@gmail.com>
2017-03-12 15:56:39 +0100

    Add LOAD_PROJECTS case to the projectReducer

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -2,12 +2,15 @@
 // returns the new state. Reducer has to be pure function.
 
 import {CREATE_PROJECT} from '../actions/createProjectAction';
+import {LOAD_PROJECTS_SUCCESS} from '../actions/loadProjectsAction';
 
 export default function projectReducer(state = [], action){
   switch (action.type){
     case CREATE_PROJECT:
       debugger;
       return [...state, Object.assign({}, action.payload)]; //ES6
+    case LOAD_PROJECTS_SUCCESS:
+      return action.projects; // projects which were passed in on the loadProjectsAction
     default:
       return state;
   }
