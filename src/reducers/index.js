ffe5c4be5bc96a2884f8fc7871c2a1b9b79d8287
Daniyar <ddr199305@gmail.com>
2017-03-14 21:48:02 +0100

    Add authorReducer to the rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import ProjectReducer from './projectReducer';
+import AuthorReducer from './authorReducer';
 
 const rootReducer = combineReducers({
-  projects: ProjectReducer
+  projects: ProjectReducer,
+  authors: AuthorReducer
 });
 
 export default rootReducer;
