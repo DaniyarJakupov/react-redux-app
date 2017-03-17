ec989c7e8aea1d9c245b622f1f85236ae2dc482c
Daniyar <ddr199305@gmail.com>
2017-03-17 22:06:43 +0100

    Add handling for CREATE_PROJECT and UPDATE_PROJECT actions

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -1,8 +1,9 @@
 // Reducer is a function() that takes previous state, the action being dispatch and
 // returns the new state. Reducer has to be pure function.
 
-import {CREATE_PROJECT} from '../actions/createProjectAction';
 import {LOAD_PROJECTS_SUCCESS} from '../actions/loadProjectsAction';
+import {CREATE_PROJECTS_SUCCESS} from '../actions/saveProjectAction';
+import {UPDATE_PROJECTS_SUCCESS} from '../actions/saveProjectAction';
 import initialState from './initialState';
 
 export default function projectReducer(state = initialState.projects, action){
@@ -11,6 +12,10 @@ export default function projectReducer(state = initialState.projects, action){
     //   return [...state, Object.assign({}, action.payload)]; //ES6
     case LOAD_PROJECTS_SUCCESS:
       return action.projects; // project which were passed in on the loadProjectsAction
+    case CREATE_PROJECTS_SUCCESS:
+      return [... state, Object.assign({}, action.project)];
+    case UPDATE_PROJECTS_SUCCESS:
+      return [... state.filter(project => project.id !== action.project.id), Object.assign({}, action.project)];
     default:
       return state;
   }
